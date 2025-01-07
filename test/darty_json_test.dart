eec9dd4177170ab1f15c3d02adad794b73596979
Emmanuel VAÏSSE <emmanuel.vaisse@boursorama.fr>
2025-01-07 09:58:45 +0100

    feat: Add better null safety protection by discarding instance of and replace with  alternative
    Add DCM basic support and update lints and requirements
    Updated `lints`

--- test/darty_json_test.dart
+++ test/darty_json_test.dart
@@ -34,15 +34,17 @@ void main() {
       expect(json['anint'].float, 12);
 
       expect(json['alist'].list is List<Json>, true);
-      expect(json['alist'].listValue[3].string, 'hello');
+      expect(json['alist'].listValue.elementAtOrNull(3)?.string, 'hello');
 
       expect(json['amap']['hello'].string, 'world');
 
       expect(json['amap']['hello']['doesnexists'].rawValue, null);
 
       expect(json['amap']['doesnexists'].exception?.error, JsonError.notExist);
-      expect(json['amap']['hello']['doesnexists'].exception?.error,
-          JsonError.wrongType);
+      expect(
+        json['amap']['hello']['doesnexists'].exception?.error,
+        JsonError.wrongType,
+      );
       expect(json['alist'][1000].exception?.error, JsonError.indexOutOfBounds);
       expect(json['alist']['hello'].exception?.error, JsonError.wrongType);
 
@@ -78,10 +80,7 @@ void main() {
         "alist": [1, 2, 3, "hello", "world"],
         "aintlist": [1, 2, 3],
         "amapofint": {"yo": 1, "lo": 2},
-        "amap": {
-          "hello": "world",
-          "yo": 10,
-        },
+        "amap": {"hello": "world", "yo": 10},
         "notallowed": () => "wat",
       },
     );
@@ -93,7 +92,7 @@ void main() {
     jsonFromMap['newkey'] = 'hello';
     expect(jsonFromMap['newkey'].string, 'hello');
 
-    JsonPayload jsonFromList = JsonPayload.fromList(<dynamic>[1, 2, 3, 4]);
+    JsonPayload jsonFromList = JsonPayload.fromList([1, 2, 3, 4]);
 
     jsonFromList[4] = 'hello';
 
